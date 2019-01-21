38869a37e147809dfe1586e7c30df3a3b0b0c4ad
darkhist <qmsalas@iastate.edu>
2019-01-20 22:45:05 -0600

    init commit

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <h1> hello world :~) </h1>
+);
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
